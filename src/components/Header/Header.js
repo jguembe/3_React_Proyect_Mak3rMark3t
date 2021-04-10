9a1e57227126140763c9bd871ee3dd079ecb7de0
bitcreator <bitcreator@elude.in>
2021-04-10 20:57:08 +0200

    introducido logo favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,14 @@ import './Header.css';
 class Header extends React.Component {
     render() {
         return (
-            <div className="Header">
+            <div className="Header row justify-content-center">
+                <div className="col-md-2">
+                  <img className="img-responsive img-logo" src="/favicon.ico"/>
+                </div>
+                <div className="col-md-4">
                 <h1>{this.props.titulo}</h1>
                 <p>¡Haz tu pedido ahora y recibelo en 24h!</p>
+                </div>
             </div>
         )
     }
